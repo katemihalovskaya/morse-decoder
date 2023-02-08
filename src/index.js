41b3a38885882b6f9783663062f6f69dd95ed3f9
Kate Mihalovskaya <kate.mihalovskaya@gmail.com>
2023-02-08 23:16:30 +0100

    feat: implement morser-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = '';
+    for (let start = 0; ; start += 10) {
+        let letterInDecimal = expr.substr(start, 10);
+        if(letterInDecimal) {
+            let letterInMorse = '';
+            if(letterInDecimal.includes('*')) {
+                word += ' ';
+            } else {
+                for(let i = letterInDecimal.length - 1; i >= 0 ; i = i - 2) {
+                    let decimalSymbol = `${letterInDecimal[i-1]}${letterInDecimal[i]}`;
+                    if(decimalSymbol === '10') {
+                        letterInMorse += '.';
+                    } else if (decimalSymbol === '11') {
+                        letterInMorse += '-';
+                    } else {
+                        break;
+                    }
+                }
+                letterInMorse = letterInMorse.split('').reverse().join('');
+                word += MORSE_TABLE[letterInMorse];
+            }
+        } else {
+            break;
+        }
+    }
+    return word;
 }
 
 module.exports = {
